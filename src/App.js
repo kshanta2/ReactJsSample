dcc4b03f3cdf14525651e77d5020797bd89fc99b
Kumar Shantanu <kumar1.shantanu@hotmail.com>
2021-09-15 16:39:01 +0530

    Sample reaact app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn Raect'></Todo>
+      <Todo text='Master Raect'></Todo>
+      
+    </div>
+  );
+}
+
+export default App;
